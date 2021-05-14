6bb1eaa72189ad51a894fac3d78d69d65e94dd11
Andrés García <aardila@colsif.com.co>
2021-05-14 16:52:26 -0500

    Were implement handle error with assert in challenge 3

--- semana-1/challenge-3.py
+++ semana-1/challenge-3.py
@@ -1,25 +1,29 @@
 MILE = 1.609344
 
 def convert(convert_to, distance):
-    distance = float(distance)
-    if(convert_to == '1'):
-        print(str(distance) + ' kilometers are ' + str(distance / MILE))
-    elif(convert_to == '2'):
-        print(str(distance) + ' miles are ' + str(MILE * distance))
-    else:
-        print('Option is not valid')
+    try:
+        assert str(convert_to).isnumeric(), 'You must entry some next options [1] Kilometers to Miles - [2] Miles to Kilometers'
+        assert str(distance).isnumeric(), 'You must entry a number'
+
+        distance = float(distance)
+        if(convert_to == '1'):
+            print(str(distance) + ' kilometers are ' + str(distance / MILE))
+        elif(convert_to == '2'):
+            print(str(distance) + ' miles are ' + str(MILE * distance))
+        else:
+            print('Option is not valid')
+    except AssertionError as ae:
+        print(ae)
 
 
 def run():
     convert_to = input('''
 Converter of miles and kilometers
 
-What type conversion do you want?
-
 [1] Kilometers to Miles
 [2] Miles to Kilometers
 
-''')
+What type conversion do you want? ''')
 
     distance = input('Entry distance: ')
 
